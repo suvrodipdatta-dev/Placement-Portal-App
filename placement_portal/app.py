9a6f0163d315c09be7b29eebca2d81379ef297b1
Suvrodip Datta <suvrodipdatta@gmail.com>
2026-02-14 13:16:53 +0530

    Milestone-0 PPA-MAD-1

--- placement_portal/app.py
+++ placement_portal/app.py
@@ -1,4 +1,8 @@
 
-
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///placement.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
